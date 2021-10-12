f72f3dc113bc7b735c77f36ad6b0d90dca8e968d
Hector Rodriguez Jr <hector.rodriguez3@gmail.com>
2021-10-12 09:54:39 -0500

    added explore.py

--- prepare.py
+++ prepare.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from datetime import datetime
+from sklearn.model_selection import train_test_split
 
-def prepare(df):
+def cleaned(df):
     df = df.drop(columns=['Crash ID', 'Average Daily Traffic Amount', 'Average Daily Traffic Year', 'Highway Number',
                      'Surface Condition', 'Surface Type', 'Vehicle Hit and Run Flag',
                      'Person Blood Alcohol Content Test Result', 'Person Drug Test Result',
@@ -62,3 +63,16 @@ def prepare(df):
 
     return df
 
+def train_validate_test_split(df, target, seed=123):
+    '''
+    This function takes in a dataframe, the name of the target variable
+    (for stratification purposes), and an integer for a setting a seed
+    and splits the data into train, validate and test. 
+    Test is 20% of the original dataset, validate is .30*.80= 24% of the 
+    original dataset, and train is .70*.80= 56% of the original dataset. 
+    The function returns, in this order, train, validate and test dataframes. 
+    '''
+    
+    train_validate, test = train_test_split(df, test_size=0.2, random_state=seed, stratify=df[target])
+    train, validate = train_test_split(train_validate, test_size=0.3, random_state=seed, stratify=train_validate[target])
+    return train, validate, test
